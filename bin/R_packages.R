216b95812f71acab098ba3012110309042e27df1
Verena1992 <verena.gebhart@rolmail.net>
2021-11-08 22:54:10 +0100

    conda enviro with r packages

---
+++ bin/R_packages.R
@@ -0,0 +1,25 @@
+#install packages
+
+#Installing R Package Dependencies for R Markdown: 'highr', 'knitr', 'markdown', 'rmarkdown', 'tinytex', 'xfun'
+
+install.packages(c('highr', 'knitr', 'rmarkdown', 'markdown', 'tinytex', 'xfun'))
+#update.packages(ask=F)
+
+
+library(highr)
+library(knitr)
+library(rmarkdown)
+library(markdown)
+library(tinytex)
+library(xfun)
+sessionInfo()
+#markdown_1.1   knitr_1.36     rmarkdown_2.11
+
+#attached base packages:
+ # [1] stats     graphics  grDevices utils     datasets  methods   base     
+
+#other attached packages:
+ # [1] xfun_0.28      tinytex_0.35   markdown_1.1   rmarkdown_2.11 knitr_1.36     highr_0.9     
+
+#loaded via a namespace (and not attached):
+#  [1] compiler_4.1.1  fastmap_1.1.0   htmltools_0.5.2 tools_4.1.1     digest_0.6.28   rlang_0.4.12    evaluate_0.14  
